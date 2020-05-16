d9e8a6d8eae33e40472a84002e8afd0d7fc521a0
Jake Alexander <jalexander91196@gmail.com>
2020-05-15 21:04:05 -0400

    Updates Ready to Send to Partner

---
+++ src/components/Data.js
@@ -0,0 +1,102 @@
+export const bookstoreProducts = [
+    {
+        id: 1,
+        title: "Animal Farm",
+        img: "img/AF.jpg",
+        price: 10,
+        info:
+            "Info unavailable at this time. We are sorry for the inconvience",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "The Shack",
+        img: "img/TS.jpg",
+        price: 10,
+        info:
+            "Info unavailable at this time. We are sorry for the inconvience",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "The Art of War",
+        img: "img/AofW.jpg",
+        price: 10,
+        info:
+            "Info unavailable at this time. We are sorry for the inconvience",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "The Magic Treehouse Box Set",
+        img: "img/MTH.jpg",
+        price: 30,
+        info:
+            "Info unavailable at this time. We are sorry for the inconvience",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Calvin and Hobbes",
+        img: "img/CAH.jpg",
+        price: 12,
+        info:
+            "Info unavailable at this time. We are sorry for the inconvience",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Nostradamus",
+        img: "img/Nos.jpg",
+        price: 20,
+        info:
+            "Info unavailable at this time. We are sorry for the inconvience",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "The Boy in the Striped Pajamas",
+        img: "img/TBSP.jpg",
+        price: 11,
+        info:
+            "Info is unavailable at this time. Sorry",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "5 Nights at Freddy's",
+        img: "img/FNF.jpg",
+        price: 18,
+        info:
+            "Info is unavailable at this time. Sorry",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+]
+
+export const detailProduct = {
+    id: 1,
+    title: "Animal Farm",
+    img: "img/AF.jpg",
+    price: 10,
+    info:
+        "info is unavailable at this time. We are sorry for the inconvience",
+    inCart: false,
+    count: 0,
+    total: 0
+};
